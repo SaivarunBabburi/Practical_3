ed28f243c0f683e65c488e788547af1d798bb0e8
SaivarunBabburi <Sb24adu@herts.ac.uk>
2024-11-06 04:55:55 +0530

    commit by varun

---
+++ Practical3.R
@@ -0,0 +1,12 @@
+library(readr)
+survey_data <- read_csv("survey_data.csv")
+df<-survey_data
+names(df)[7] <- "award"
+names(df)[8] <- "gender"
+names(df)[9] <- "height"
+names(df)[10] <- "continent"
+names(df)[11] <- "pe_minutes"
+names(df)[12] <- "miles"
+names(df)[13] <- "gbptransport"
+names(df)[14] <- "get2campusminutes"
+names(df)[15] <- "gbpfood"
